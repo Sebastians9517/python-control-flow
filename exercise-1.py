8e4f2ceaf59ba08b1cd22158e40df1f4e5234751
sebastians9517 <sebastians9517@gmail.com>
2021-01-26 19:22:04 -0600

    exercise-1 completed

--- exercise-1.py
+++ exercise-1.py
@@ -13,18 +13,6 @@
 #         For example, if some_char in 'abc':
 
 
-# c = input("Please enter a letter from the alphabet (a - z or A - Z)")
-
-# if (c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u' or
-#     c == 'A' or c == 'E' or c == 'I' or c == 'O' or c == 'U'):
-#     print(f"The letter {c} is a vowel")
-# else:
-#     print(f"The letter {c} is a consonant")
-
-
-
-# Alternatively:
-
 def is_a_vowel(c):
     vowels = "aeiouAEIOU"
     if c in vowels:
@@ -33,3 +21,15 @@ def is_a_vowel(c):
         print(f"The letter {c} is a consonant")
 
 print(is_a_vowel(input("Please enter a letter from the alphabet (a - z or A - Z)")))
+
+
+
+# Alternatively:
+
+# c = input("Please enter a letter from the alphabet (a - z or A - Z)")
+
+# if (c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u' or
+#     c == 'A' or c == 'E' or c == 'I' or c == 'O' or c == 'U'):
+#     print(f"The letter {c} is a vowel")
+# else:
+#     print(f"The letter {c} is a consonant")
